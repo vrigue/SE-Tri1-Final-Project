cde879c6cb5fd06243ebf57ff73aa1ac4259c260
J-Tell <fairyq007@hotmail.com>
2023-12-02 17:04:22 -0500

    tuk tuk movement with enums

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,36 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // enum Postion
-    // {
-    //     Left,
-    //     Middle,
-    //     Right
-    // }
-    // public void Update()
-    // {
-    //     Position pos = Position.Middle;
-    //     switch(pos)
-    //     {
-    //         case Input.GetKeyDown(KeyCode.LeftArrow):
-    //             pos = Position.Left;
-    //             break;
-    //     }
-    // }
-
-
     public float speed = 20;
-    // //private float turnSpeed = 35;
     private float horizontalInput;
     private float forwardInput;
     public float moveSpeed = 3.0f;
     public float leftRightSpeed = 4.0f;
 
-    // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    public enum Movement {
+        None,
+        Left,
+        Right
+    }
 
     void Update()
     {
@@ -41,28 +22,48 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        // // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        // if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-        //      transform.position = new Vector3(-5,0,6);
-        // }
-        // if (Input.GetKeyDown(KeyCode.RightArrow)) {
-        //     transform.position = new Vector3(5,0,6);
-        // }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            if (this.gameObject.transform.position.x > LevelBoundary.leftSide) {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * 1.0f);
-            }
+        Movement move = Movement.None;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move = Movement.Left;
         }
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            if (this.gameObject.transform.position.x < LevelBoundary.rightSide) {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1.0f);
-            }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move = Movement.Right;
         }
+
+        switch (move)
+        {
+  
+            case Movement.Left:
+                if (this.gameObject.transform.position.x > LevelBoundary.leftSide) {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * 1.0f);
+                }
+                break;
+
+            case Movement.Right:
+                if (this.gameObject.transform.position.x < LevelBoundary.rightSide) {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1.0f);
+                }
+                break;
+        }
+
+
+        // if (Input.GetKey(KeyCode.LeftArrow)) {
+        //     if (this.gameObject.transform.position.x > LevelBoundary.leftSide) {
+        //         transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * 1.0f);
+        //     }
+        // }
+        // if (Input.GetKey(KeyCode.RightArrow)) {
+        //     if (this.gameObject.transform.position.x < LevelBoundary.rightSide) {
+        //         transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1.0f);
+        //     }
+        // }
 
     }
+
 }
-
-
-
